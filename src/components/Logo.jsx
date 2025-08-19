44c12e4e916ab664cf7d4efc1e4500b70a4b7ad5
elysthetruechi <kaluelizabeth29@gmail.com>
2025-08-19 10:34:09 +0100

    .

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './LogoRow.css';
+// Import all images
+import comresk from '../assets/Image_fx (9) 1.png';
+import spielonic from '../assets/Image_fx (36) 1.png';
+import guiltbin from '../assets/Image_fx (35) 1.png';
+import marvaty from '../assets/Image_fx (34) 1.png';
+import orblo from '../assets/Image_fx (33) 1.png';
 
 const LogoRow = () => {
   return (
     <div className="logo-row-wrapper">
       <div className="logo-row">
-        <img src="../assets/Image_fx (9) 1.png" alt="comresk" className="logon" />
-        <img src="../assets/Image_fx (36) 1.png" alt="spielonic" className="logon" />
-        <img src="../assets/Image_fx (35) 1.png" alt="guiltbin" className="logon" />
-        <img src="../assets/Image_fx (34) 1.png" alt="marvaty" className="logon" />
-        <img src="../assets/Image_fx (33) 1.png" alt="orblo" className="logon" />
-        <img src="../assets/Image_fx (9) 1.png" alt="comresk" className="logon" />
-        <img src="../assets/Image_fx (36) 1.png" alt="spielonic" className="logon" />
-        <img src="../assets/Image_fx (35) 1.png" alt="guiltbin" className="logon" />
-        <img src="../assets/Image_fx (34) 1.png" alt="marvaty" className="logon" />
-        <img src="../assets/Image_fx (33) 1.png" alt="orblo" className="logon" />
+        <img src={comresk} alt="comresk" className="logon" />
+        <img src={spielonic} alt="spielonic" className="logon" />
+        <img src={guiltbin} alt="guiltbin" className="logon" />
+        <img src={marvaty} alt="marvaty" className="logon" />
+        <img src={orblo} alt="orblo" className="logon" />
+        <img src={comresk} alt="comresk" className="logon" />
+        <img src={spielonic} alt="spielonic" className="logon" />
+        <img src={guiltbin} alt="guiltbin" className="logon" />
+        <img src={marvaty} alt="marvaty" className="logon" />
+        <img src={orblo} alt="orblo" className="logon" />
       </div>
     </div>
   );
