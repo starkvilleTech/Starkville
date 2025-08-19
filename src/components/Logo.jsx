28ceb3d1c7939e447637289e417f1f7f0bf4d653
elysthetruechi <kaluelizabeth29@gmail.com>
2025-08-19 10:22:17 +0100

    .

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,16 +5,16 @@ const LogoRow = () => {
   return (
     <div className="logo-row-wrapper">
       <div className="logo-row">
-        <img src="src/assets/Image_fx (9) 1.png" alt="comresk" className="logon" />
-        <img src="src/assets/Image_fx (36) 1.png" alt="spielonic" className="logon" />
-        <img src="src/assets/Image_fx (35) 1.png" alt="guiltbin" className="logon" />
-        <img src="src/assets/Image_fx (34) 1.png" alt="marvaty" className="logon" />
-        <img src="src/assets/Image_fx (33) 1.png" alt="orblo" className="logon" />
-        <img src="src/assets/Image_fx (9) 1.png" alt="comresk" className="logon" />
-        <img src="src/assets/Image_fx (36) 1.png" alt="spielonic" className="logon" />
-        <img src="src/assets/Image_fx (35) 1.png" alt="guiltbin" className="logon" />
-        <img src="src/assets/Image_fx (34) 1.png" alt="marvaty" className="logon" />
-        <img src="src/assets/Image_fx (33) 1.png" alt="orblo" className="logon" />
+        <img src="../assets/Image_fx (9) 1.png" alt="comresk" className="logon" />
+        <img src="../assets/Image_fx (36) 1.png" alt="spielonic" className="logon" />
+        <img src="../assets/Image_fx (35) 1.png" alt="guiltbin" className="logon" />
+        <img src="../assets/Image_fx (34) 1.png" alt="marvaty" className="logon" />
+        <img src="../assets/Image_fx (33) 1.png" alt="orblo" className="logon" />
+        <img src="../assets/Image_fx (9) 1.png" alt="comresk" className="logon" />
+        <img src="../assets/Image_fx (36) 1.png" alt="spielonic" className="logon" />
+        <img src="../assets/Image_fx (35) 1.png" alt="guiltbin" className="logon" />
+        <img src="../assets/Image_fx (34) 1.png" alt="marvaty" className="logon" />
+        <img src="../assets/Image_fx (33) 1.png" alt="orblo" className="logon" />
       </div>
     </div>
   );
